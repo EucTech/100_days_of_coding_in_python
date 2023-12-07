4995877f2b9f24e0eaadcc9b491cfa64547686dc
EucTech <euccreative@gmail.com>
2023-12-07 12:48:35 +0100

    data_types.py

---
+++ day_2_of_coding_in_python/data_types.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Data Types
+
+1 String
+2 Interger
+3 Float
+4 Boolean
+
+You can use type() to check the type of the integers
+"""
+
+# String
+"Hello"
+
+# Subscript print the character at index 0 
+print("Hello"[0])
+
+"123" # This is now a string not a number because it is inside a string
+
+# Integer
+123
+print(344 + 234)
+
+# Float 
+print(3.66)
