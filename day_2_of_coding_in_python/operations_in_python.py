8bbb06cd3565fe066c2cd5615cbc5a2ba8952b75
EucTech <euccreative@gmail.com>
2023-12-07 12:49:06 +0100

    operations_in_python.py

---
+++ day_2_of_coding_in_python/operations_in_python.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+3 + 5 # addition
+6 - 2 # substraction
+3 * 2 # multiplication
+6 / 2 # division
+2 ** 2 # Exponent, raise to power of
+10 % 3 # floor
+
+'''
+()
+**
+*
+/
++
+-
+'''
+
+print(3 * (3 + 3) / 3 - 3)
