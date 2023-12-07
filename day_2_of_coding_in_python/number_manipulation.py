711e28e7dfb9b5162711d923e379720731423def
EucTech <euccreative@gmail.com>
2023-12-07 12:48:55 +0100

    number_manipulation.py

---
+++ day_2_of_coding_in_python/number_manipulation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+print(round(10 / 3))
+print(round(10 / 3, 3)) # Round it up to 3 decimal places
+
+print(10 // 3) # It will print an integer 3 instead of a float
+
+print(10 % 3) # To print the reminder
+
+score = 0
+
+score += 1
+print(score)
+
+
+# f-String
+print(f"Your score is {score}")
