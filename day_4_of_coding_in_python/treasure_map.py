1d312e4d4114f7b522615a0788f01b7de13e24b9
EucTech <euccreative@gmail.com>
2023-12-11 12:04:39 +0100

    treasure_map.py

---
+++ day_4_of_coding_in_python/treasure_map.py
@@ -0,0 +1,17 @@
+line1 = ["📦", "📦", "📦"]
+line2 = ["📦", "📦", "📦"]
+line3 = ["📦", "📦", "📦"]
+
+map = (line1, line2, line3)
+
+print("Hiding your treasure! 📥 marks the spot.")
+
+position = input()
+
+letter = position[0].lower()
+abc = ["a", "b", "c"]
+letter_index = abc.index(letter)
+number_index = int(position[1]) - 1
+map[number_index][letter_index] = "📥"
+
+print(f"{line1}\n{line2}\n{line3}")
