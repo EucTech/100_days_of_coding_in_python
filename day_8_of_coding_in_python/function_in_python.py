9dba7bf579dabaa057801fa232bac8a514e6ecbb
EucTech <euccreative@gmail.com>
2023-12-18 11:27:57 +0100

    function_in_python.py

---
+++ day_8_of_coding_in_python/function_in_python.py
@@ -0,0 +1,12 @@
+# def great():
+#     name = input("Enter your name: ")
+#     Value = "Hello"
+#     print(f"{Value} {name}")
+    
+# great()
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+    
+greet_with(location="Rwanda", name="Uche")
